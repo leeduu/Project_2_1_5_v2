cc1ce82be6b5d278bbc95a21027065921b19abf8
leeduu <lidiya_by@mail.ru>
2023-01-06 21:50:15 +0300

    1

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,11 @@
 package koschei.config;
 
+import koschei.models.Duck5;
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,4 +18,10 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+//    @Bean
+//    public static Duck5 getDuck(Egg6 egg) {
+//        return new Duck5(egg);
+//    }
+
 }
